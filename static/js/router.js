5281b58be26e9359f494f77a9a8e4750388b56bf
badil3in <badil3in@googlemail.com>
2026-04-01 01:10:48 +0200

    logo, responsive css refinement

--- static/js/router.js
+++ static/js/router.js
@@ -1,11 +1,10 @@
-// configure flags
+// configure nunjucks
 nunjucks.configure('/templates', { 
     autoescape: true,
     async: true 
 });
 
 // render function
-
 async function render() {
     const page = location.hash.replace('#/', '') || 'index';
 
@@ -22,20 +21,31 @@ async function render() {
         document.getElementById('app').innerHTML = html;
         activeNav(page);
     });
-    // document.getElementById('app').innerHTML = html;
-    // console.log("Render:", `${page}.njk`);
-
-    // activeNav(page);
 }
 
 // toggle nav active
-
 function activeNav(page) {
     document.querySelectorAll('.nav-link').forEach(link => {
         link.classList.toggle('active', link.getAttribute('href') === `#/${page}`);
     });
 }
 
+// close nav burgermenu on outsideclick
+document.addEventListener('click', function(event) {
+    const navbar = document.getElementById('navbarSupportedContent');
+    const toggler = document.querySelector('.navbar-toggler');
+
+    // navbar element shown
+    // click nicht auf navbar
+    // click nicht auf toggler
+    if(navbar.classList.contains('show') 
+        && !navbar.contains(event.target) 
+        && !toggler.contains(event.target)) {
+            
+        // click auf toggler auslösen
+        toggler.click();
+    }
+})
 
 window.addEventListener('hashchange', render);
 render();
